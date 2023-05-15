2b9608106be749662f373a2d1643b3cb8cdcb25b
PavelPisaryk <120409118+PavelPisaryk@users.noreply.github.com>
2023-05-15 20:46:59 +0300

    Add files via upload

---
+++ Ass1.2/Ass1.2/lab2.asm
@@ -0,0 +1,38 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+.data
+a byte 0
+b byte 0
+f byte 1 ;почему то на c он выдаёт ошибки
+d byte 0
+t1 byte ?
+s byte ?
+
+ExitProcess PROTO, dwExitCode:byte
+
+.code
+main PROC
+
+mov al, a
+xor al, 1
+or al, b
+or al, f
+mov ah, d
+xor ah, 1
+and al, ah
+xor al, 1
+mov t1, al
+
+mov al, b
+or al, f
+xor al, 1
+and al, a
+or al, d
+mov s, al
+
+INVOKE ExitProcess,0
+
+main ENDP
+END main
